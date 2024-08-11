64e993ae46fe861df64052e9c3621075ffc92f09
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 20:56:36 -0300

    Resolved test 1 and 2 with a for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1,num2) {
+    let result = 0
+    for (let i = num1 ; i <= num2; i++) {
+       result += i; 
+    }
+    return result;
+    
+    }
+;
 
 // Do not edit below this line
 module.exports = sumAll;
