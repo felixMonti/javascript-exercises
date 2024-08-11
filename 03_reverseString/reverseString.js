6aac872df093809e99d9fdd330ed3aa6003a1aa2
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 19:53:34 -0300

    Create an array with the characters of the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = [...str]
+    console.log(arr)
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
