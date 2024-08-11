50ba836e232d8dd4a3e6a2fe3eb7c46bba4b78b1
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 20:59:59 -0300

    Resolved test 3 with an if statement

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,17 @@
 const sumAll = function(num1,num2) {
     let result = 0
-    for (let i = num1 ; i <= num2; i++) {
-       result += i; 
+    if (num1 < num2) {
+        for (let i = num1 ; i <= num2; i++) {
+            result += i; 
+         }
+         return result;
+    } else{
+        for (let i = num2 ; i <= num1; i++) {
+            result += i; 
+         }
+         return result;
     }
-    return result;
+
     
     }
 ;
