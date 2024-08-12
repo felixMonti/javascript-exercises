1a0e28bd2c4994a694ac9c240b62b47e0b9ff823
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 21:03:32 -0300

    Added ERROR for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
 const sumAll = function(num1,num2) {
     let result = 0
-    if (num1 < num2) {
+
+    if(num1 < 0 || num2 < 0 ){
+        return "ERROR"
+    } else if (num1 < num2) {
         for (let i = num1 ; i <= num2; i++) {
             result += i; 
          }
