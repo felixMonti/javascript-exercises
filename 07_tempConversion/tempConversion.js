adb7dfd21ae24c7a57bd9deedd049ee76ad45378
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 21:43:26 -0300

    Added factors to return one decimal number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function(temp) {
   let tempCelcius = (temp - 32)*5/9 
-  return Math.round(tempCelcius);
+  return Math.round(tempCelcius* 10) / 10;
   
 };
 
 const convertToFahrenheit = function(temp) {
   let tempFarenheit = (temp*9/5) + 32 
-  return Math.round(tempFarenheit);
+  return Math.round(tempFarenheit* 10) / 10;
 };
 
 // Do not edit below this line
