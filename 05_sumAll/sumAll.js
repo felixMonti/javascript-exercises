414751228d2391164a429d9d728bcb48bb0f80f0
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 21:14:23 -0300

    Solved other tests by checking that Arguments are integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(num1,num2) {
     let result = 0
 
-    if(num1 < 0 || num2 < 0 ){
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) ||!Number.isInteger(num2) ){
         return "ERROR"
     } else if (num1 < num2) {
         for (let i = num1 ; i <= num2; i++) {
