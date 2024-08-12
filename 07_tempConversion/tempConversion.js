68ce813ada627645f135f5294a74eb2661aa628a
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-08-11 21:41:27 -0300

    Added equations to functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempCelcius = (temp - 32)*5/9 
+  return Math.round(tempCelcius);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempFarenheit = (temp*9/5) + 32 
+  return Math.round(tempFarenheit);
 };
 
 // Do not edit below this line
