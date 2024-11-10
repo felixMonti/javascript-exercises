dacaa97c3a31ae534b2f012740030c42e3b12162
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-11-10 15:46:33 -0300

    excercise tested

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const stringProcesada = string
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanum.includes(character))
+        .join("");
+
+    const reversed = stringProcesada.split("").reverse().join("");
+
+    return stringProcesada === reversed;
 };
+
+console.log(palindromes("alaM"))
 
 // Do not edit below this line
 module.exports = palindromes;
