73afda9148e85c90e0e26c057a1196b38c69fae9
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-11-10 20:54:10 -0300

    done and tested

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    
+    let count;
+    if (typeof arg !== 'number') {
+        count = parseInt(arg)
+    } else {
+        count = arg
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let primero = 1;
+    let segundo = 0;
+
+    for(let i = 2; i <= count; i++){
+        let fib = primero + segundo;
+        segundo = primero;
+        primero = fib;
+    }
+
+    return primero;
 };
 
 // Do not edit below this line
